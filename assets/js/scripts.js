c8e436df396dae1fc73c2d79e01089c6dcca1f18
Fernanda <fernandaavellorocha@gmail.com>
2023-05-05 12:02:48 -0400

    Proyecto chat- componentes + librerias

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+function goToChat() {
+  window.location.href = '/chatView.html'
+}
+function goHome() {
+  window.location.href = '/index.html'
+}
+function changeCard() {
+  let element = document.getElementById('bold__text');
+  let actualConversation = document.getElementById('gojo-conversation')
+  element.style.fontWeight = '400';
+  element.style.fontStyle = 'normal';
+  element.style.opacity = 1;
+  actualConversation.classList.add('actual__conversation__selected')
+}
